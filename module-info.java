10e84aa508ae96b63552f16039d2f9880233b863
Moises Yacila <moises.yacila.cs@gmail.com>
2020-06-10 19:31:47 -0700

    add module-info.java

---
+++ module-info.java
@@ -0,0 +1,15 @@
+module BodyMassIndex {
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires transitive javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+//	requires javafx.swt;
+	requires javafx.web;
+	
+	
+	
+	//opens JavaFXDemo to javafx.fxml;
+	exports bmi;
+}
